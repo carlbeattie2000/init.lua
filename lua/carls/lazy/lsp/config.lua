19e8d83e4e2f583339da6d70e2d74ae307bb099b
Carl Beattie <contact.carlbeattie@gmail.com>
2025-04-11 14:49:29 +0100

    chore: support ts, ts-react and js-react files with conform

--- lua/carls/lazy/lsp/config.lua
+++ lua/carls/lazy/lsp/config.lua
@@ -14,6 +14,10 @@ M.setup = function(_, opts)
 		formatters_by_ft = {
 			lua = { "stylua" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
 			sh = { "beautysh" },
 			python = { "black" },
 		},
